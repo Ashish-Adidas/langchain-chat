2da7d995fd24e741bb275c0afab17ee95b461c3b
ashishdatawhiz <97599701+Ashish-Adidas@users.noreply.github.com>
2023-10-10 09:51:55 +0000

    Langchain changes

--- 2_news_research_tool_project/main.py
+++ 2_news_research_tool_project/main.py
@@ -1,60 +1,72 @@
-import os
 import streamlit as st
 import pickle
 import time
 from langchain import OpenAI
 from langchain.chains import RetrievalQAWithSourcesChain
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.text_splitter import RecursiveCharacterTextSplitter, CharacterTextSplitter
+from langchain.chains.question_answering import load_qa_chain
+from langchain.callbacks import get_openai_callback
 from langchain.document_loaders import UnstructuredURLLoader
+from PyPDF2 import PdfReader
 from langchain.embeddings import OpenAIEmbeddings
 from langchain.vectorstores import FAISS
+from utilities import utils
 
 from dotenv import load_dotenv
 load_dotenv()  # take environment variables from .env (especially openai api key)
 
-st.title("RockyBot: News Research Tool 📈")
-st.sidebar.title("News Article URLs")
+st.title("Research Chat Tool 📈")
 
-urls = []
-for i in range(3):
-    url = st.sidebar.text_input(f"URL {i+1}")
-    urls.append(url)
+# Add a new tab for chatting with PDFs
+tabs = st.tabs(["Chat with URLs", "Chat with PDF"])
 
-process_url_clicked = st.sidebar.button("Process URLs")
-file_path = "faiss_store_openai.pkl"
+# Chat with URLs tab
+with tabs[0]:
+    urls = []
+    for i in range(3):
+        url = st.sidebar.text_input(f"URL {i+1}")
+        urls.append(url)
 
-main_placeholder = st.empty()
-llm = OpenAI(temperature=0.9, max_tokens=500)
+    process_url_clicked = st.sidebar.button("Process URLs")
+    file_path = "faiss_store_openai_urls.pkl"
 
-if process_url_clicked:
-    # load data
-    loader = UnstructuredURLLoader(urls=urls)
-    main_placeholder.text("Data Loading...Started...✅✅✅")
-    data = loader.load()
-    # split data
-    text_splitter = RecursiveCharacterTextSplitter(
-        separators=['\n\n', '\n', '.', ','],
-        chunk_size=1000
-    )
-    main_placeholder.text("Text Splitter...Started...✅✅✅")
-    docs = text_splitter.split_documents(data)
-    # create embeddings and save it to FAISS index
-    embeddings = OpenAIEmbeddings()
-    vectorstore_openai = FAISS.from_documents(docs, embeddings)
-    main_placeholder.text("Embedding Vector Started Building...✅✅✅")
-    time.sleep(2)
+    main_placeholder = st.empty()
+    llm = OpenAI(temperature=0.9, max_tokens=500)
 
-    # Save the FAISS index to a pickle file
-    with open(file_path, "wb") as f:
-        pickle.dump(vectorstore_openai, f)
+    if process_url_clicked:
+        # load data
+        loader = UnstructuredURLLoader(urls=urls)
+        main_placeholder.text("Data Loading...Started...✅✅✅")
+        data = loader.load()
 
-query = main_placeholder.text_input("Question: ")
-if query:
-    if os.path.exists(file_path):
-        with open(file_path, "rb") as f:
-            vectorstore = pickle.load(f)
+        # split data
+        text_splitter = RecursiveCharacterTextSplitter(
+            separators=['\n\n', '\n', '.', ','],
+            chunk_size=1000
+        )
+        main_placeholder.text("Text Splitter...Started...✅✅✅")
+        docs = text_splitter.split_documents(data)
+
+        # create embeddings and save it to FAISS index
+        embeddings = OpenAIEmbeddings()
+        vectorstore_openai = FAISS.from_documents(docs, embeddings)
+        main_placeholder.text("Embedding Vector Started Building...✅✅✅")
+        time.sleep(2)
+
+        # Save the FAISS index to a pickle file
+        with open(file_path, "wb") as f:
+            pickle.dump(vectorstore_openai, f)
+
+    query = main_placeholder.text_input("Question: ")
+    if query:
+        if os.path.exists(file_path):
+            with open(file_path, "rb") as f:
+                vectorstore = pickle.load(f)
+
             chain = RetrievalQAWithSourcesChain.from_llm(llm=llm, retriever=vectorstore.as_retriever())
+
             result = chain({"question": query}, return_only_outputs=True)
+
             # result will be a dictionary of this format --> {"answer": "", "sources": [] }
             st.header("Answer")
             st.write(result["answer"])
@@ -67,6 +79,32 @@ if query:
                 for source in sources_list:
                     st.write(source)
 
+# Chat with PDF tab
+with tabs[1]:
+    pdf_file = st.file_uploader("Upload PDF:")
 
+    if pdf_file is not None:
+        # Extract the text from the PDF file
+        pdf_reader = PdfReader(pdf_file)
+        text = ""
+        for page in pdf_reader.pages:
+            text += page.extract_text()
+        knowledgeBase = utils.process_text(text)
 
-
+    
+        query = st.text_input('Ask a question to the PDF')
+        cancel_button = st.button('Cancel')
+        
+        if cancel_button:
+            st.stop()
+        
+        if query:
+            docs = knowledgeBase.similarity_search(query)
+        llm = OpenAI()
+        chain = load_qa_chain(llm, chain_type='stuff')
+            
+        with get_openai_callback() as cost:
+            response = chain.run(input_documents=docs, question=query)
+            print(cost)
+                
+        st.write(response)
---
+++ 2_news_research_tool_project/utilities/utils.py
@@ -0,0 +1,27 @@
+from dotenv import load_dotenv
+import os
+from PyPDF2 import PdfReader
+import streamlit as st
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain import FAISS
+from langchain.chains.question_answering import load_qa_chain
+from langchain.llms import OpenAI
+from langchain.callbacks import get_openai_callback
+
+def process_text(text):
+    # Split the text into chunks using Langchain's CharacterTextSplitter
+    text_splitter = CharacterTextSplitter(
+        separator="\n",
+        chunk_size=1000,
+        chunk_overlap=200,
+        length_function=len
+    )
+    chunks = text_splitter.split_text(text)
+    
+    # Convert the chunks of text into embeddings to form a knowledge base
+    embeddings = OpenAIEmbeddings()
+    knowledgeBase = FAISS.from_texts(chunks, embeddings)
+    
+    return knowledgeBase
+
